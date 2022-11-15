68e4b5e5dc4cb5891dee06cdad065567ae8cfec7
ModestFexty <117993287+ModestFexty@users.noreply.github.com>
2022-11-15 12:52:42 +0300

    Add files via upload

---
+++ 2/1.pas
@@ -0,0 +1,14 @@
+﻿const n = 5;
+var a:array[1..n] of integer;
+begin
+  var b:string;
+  print('введите последовательность');
+  var c:=readstring();
+  print('введите строку');
+  for var i:= 1 to n do begin
+    readln(A[i]);
+    b:=b+A[i].ToString;
+  end;
+  if (pos(c,b)>0) then print('da')
+  else print('net');
+end.
---
+++ 2/2.pas
@@ -0,0 +1,12 @@
+﻿var str:string;
+    i:integer;
+    f:boolean;
+begin
+  readstring(str);
+  i:=1;
+  f:=true;
+  while(i<=length(str) div 2) and f do
+    if str[i] <> str[length(str)+1-i] then f:=false
+  else i:=i+1;
+  if f then print('да') else print('нет');
+end.
